ba9ed7094a84c65825697f97fb968b68cf3cbf17
vuduyviet1110 <2001040225@s.hanu.edu.vn>
2025-06-04 12:06:20 +0700

    :saprkles:setup eslint/ lint-staged

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <button @click="count = count + 1">
+    Click me
+  </button>
+  <p>You clicked me {{ count }} times.</p>
 </template>
---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import antfu from "@antfu/eslint-config";
+
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  antfu(
+    {
+      type: "app",
+      typescript: true,
+      formatters: true,
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+    {
+      rules: {
+        "ts/no-redeclare": "off",
+        "ts/consistent-type-definitions": ["error", "type"],
+        "no-console": ["warn"],
+        "antfu/no-top-level-await": ["off"],
+        "node/prefer-global/process": ["off"],
+        "node/no-process-env": ["error"],
+        "perfectionist/sort-imports": [
+          "error",
+          {
+            tsconfigRootDir: ".",
+          },
+        ],
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "kebabCase",
+            ignore: ["README.md"],
+          },
+        ],
+      },
+    },
+  ),
+);
